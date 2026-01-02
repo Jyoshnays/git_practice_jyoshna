2b907f8e3efa3ec3c5abbe2b38fe8fde454530a2
jyoshna <jyoshnays@gmail.com>
2026-01-02 12:12:40 +0530

    dara done

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 print("hello jyoshnaa  wroldd")
 def data():
     for  range in len(5):
         print(range)
         print()
 print(data())
-=======
-print("hello wroldd")
->>>>>>> parent of ae05c66 (Update main.py)
