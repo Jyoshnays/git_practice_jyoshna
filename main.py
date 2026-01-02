83deaef4f9b0d830db3622a9f76e47efad7efd69
jyoshna <jyoshnays@gmail.com>
2026-01-02 12:07:46 +0530

    Revert "Update main.py"
    This reverts commit ae05c666e5e0766b09a6b2a50cf6c656ed9475d0.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+<<<<<<< HEAD
 print("hello jyoshnaa  wroldd")
 def data():
     for  range in len(5):
         print(range)
         print()
 print(data())
+=======
+print("hello wroldd")
+>>>>>>> parent of ae05c66 (Update main.py)
